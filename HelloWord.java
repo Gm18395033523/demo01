f1263130fb1a163a95432aff55e73ad2372e80e8
qhxwlei <qhxwlei@126.com>
2022-04-04 15:16:41 +0800

    qhxwlei

--- HelloWord.java
+++ HelloWord.java
@@ -4,14 +4,15 @@ public class HelloWorld{
 		
 		System.out.println("Hello Word!");
 		System.out.println("He he");
-<<<<<<< HEAD
+
 		System.out.println("master: content");
 
 		System.out.println("master: content");
-=======
+
 		System.out.println("hot-fix: content2");
 
->>>>>>> hot-fix
+		System.out.println("qhxlwei用户添加的新内容");
+
 	}
 	
 }
